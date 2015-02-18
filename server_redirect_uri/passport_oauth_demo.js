109a6865f7541a47b06e8ca7f8eac2f6b429ac54
Francesco Delfino <delfinof@gmail.com>
2015-02-18 16:25:48 +0100

    Added changes in passport oauth demo for better dealing with hostname abstraction (caveat: all request have to come to the same hostname otherwise login breaks...)

--- server_redirect_uri/passport_oauth_demo.js
+++ server_redirect_uri/passport_oauth_demo.js
@@ -1,10 +1,10 @@
+"use strict";
 var express = require('express'),
     app = express();
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
-
 var oauth_config= require("./oauth_config");
-var request = ("request");
+var request = require( 'promised-request' );
 
 var passport = require('passport')
     , OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
@@ -13,18 +13,18 @@ var root_hostname = "https://oauth.musixmatch.com";
 //root_hostname = "http://localhost:8181";
 //root_hostname = "http://local.musixmatch.com:8181";
 
+var strategy_options = {
+    authorizationURL: root_hostname+'/oauth/authorize',
+    tokenURL: root_hostname+'/oauth/token',
 
-var strategy = new OAuth2Strategy({
-        authorizationURL: root_hostname+'/oauth/authorize',
-        tokenURL: root_hostname+'/oauth/token',
+    clientID: oauth_config.YOUR_CLIENT_ID,
+    clientSecret: oauth_config.YOUR_SECRET,
+    callbackURL: oauth_config.YOUR_APP_REDIRECT_URI,
+    scope: "all"
+};
 
-        clientID: oauth_config.YOUR_CLIENT_ID,
-        clientSecret: oauth_config.YOUR_SECRET,
-        callbackURL: oauth_config.YOUR_APP_REDIRECT_URI,
-        scope: "all"
-    },
+var strategy = new OAuth2Strategy( strategy_options,
     function(accessToken, refreshToken, profile, done) {
-        var request = require( 'promised-request' );
         request( root_hostname+'/ws/1.1/user.get?app_id=' + oauth_config.YOUR_CLIENT_ID + "&usertoken=" + accessToken ).then( function( response )
         {
             try {
@@ -34,12 +34,11 @@ var strategy = new OAuth2Strategy({
             } catch (e) {
                 done(null,false,e);
             }
-        },function(err){
+        },function(e){
             done(null,false,e);
         });
     }
 );
-
 
 passport.use('musixmatch', strategy);
 
@@ -61,28 +60,46 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+function inject_hostname_in_strategy_config (req,res,next)
+{
+    var use_protocol = "http://";
+    if (req.headers['x-forwarded-proto'] == 'https') //azure
+        use_protocol = "https://";
+
+    if (req.headers['x-arr-ssl']) //azure
+        use_protocol = "https://";
+
+    strategy_options.callbackURL = use_protocol + req.headers.host + "/" + oauth_config.YOUR_APP_REDIRECT_URI;
+    next()
+}
+
 // Redirect the user to the OAuth 2.0 provider for authentication.  When
 // complete, the provider will redirect the user back to the application at
 //     /auth/provider/callback
-app.get('/auth/musixmatch', passport.authenticate('musixmatch'));
+app.get('/auth/musixmatch',inject_hostname_in_strategy_config, passport.authenticate('musixmatch', strategy_options));
 
+
+var callback_options = { successRedirect: '/', failureRedirect: '/failure' };
+function check_for_error_result(req,res,next) {
+    inject_hostname_in_strategy_config(req,res,function() {
+        if (typeof( req.query.error )!="undefined") {
+            console.log ("Error " + req.query.error);
+            res.redirect ('/failure');
+            res.end();
+        } else {
+            callback_options.callbackURL = strategy_options.callbackURL;
+            next();
+        }
+    });
+}
 // The OAuth 2.0 provider has redirected the user back to the application.
 // Finish the authentication process by attempting to obtain an access
 // token.  If authorization was granted, the user will be logged in.
 // Otherwise, authentication has failed.
-app.get('/auth/musixmatch/callback',
-    function(req, res, next) {
-        if (typeof( req.query.error )!="undefined") //handle error codes
-
-            res.redirect('/failure');
-         else
-            next();
-    },passport.authenticate('musixmatch',
-        { successRedirect: '/',
-        failureRedirect: '/failure' }));
+app.get('/auth/musixmatch/callback',check_for_error_result,passport.authenticate('musixmatch',callback_options) );
 
 app.get('/failure', function(req,res) {
-    res.write("Not authorized");
+    res.write("<html><body>Not authorized. Login with another account clicking <a href='/login'>here</a></body><html>");
     res.end();
 });
 
@@ -105,7 +122,7 @@ app.get('/',
         else {
             res.write(JSON.stringify(req.user));
             res.end();
-        };
+        }
 });
 
 app.listen(3000);
